aae09d3a62e219862af920903ae337dbfc417da4
22323166 <22323166@student.ciu.edu.tr>
2026-06-02 20:05:11 +0300

    Added getopt loop for argument parsing

--- cmpe351.cpp
+++ cmpe351.cpp
@@ -1,10 +1,28 @@
-/* This is code developed by <Student Name and Surname> */
+/* This is code developed by <Abdullah Taher Anwer Shaikh> */
 #include "cmpe351.h"
-#include <iostream>  
+#include <iostream>
 #include <cstdlib>
+#include <unistd.h>
+using namespace std;
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	
+int quantum = 0;
+    string input_file = "";
+    string output_file = "";
+    int opt;
+    
+    while ((opt = getopt(argc, argv, "t:f:o:")) != -1) {
+        switch (opt) {
+            case 't': quantum = stoi(optarg); break;
+            case 'f': input_file = optarg; break;
+            case 'o': output_file = optarg; break;
+            default:
+                cerr << "Usage: " << argv[0] << " -t <quantum> -f <input> -o <output>\n";
+                return 1;
+        }
+    }
+    
+    cout << "Quantum: " << quantum << " Input: " << input_file << "\n";
+return 0;
 }
