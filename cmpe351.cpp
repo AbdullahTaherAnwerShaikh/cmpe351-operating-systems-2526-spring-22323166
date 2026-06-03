0a6792ae40a568c4f060698fb5c327f60c516572
22323166 <22323166@student.ciu.edu.tr>
2026-06-03 19:48:05 +0300

    worked on reading input file and trying to build linked list

--- cmpe351.cpp
+++ cmpe351.cpp
@@ -1,9 +1,21 @@
-/* This is code developed by <Abdullah Taher Anwer Shaikh> */
+/* This is code developed by <Abdullah Taher Anwer Shaikh - 22323166> */
 #include "cmpe351.h"
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <string>
+#include <fstream>
 using namespace std;
+
+struct Process {
+    int id;
+    int priority;
+    int arrival;
+    int burst;
+    int rem;
+    int wt;
+    Process* next;
+};
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +35,45 @@ int quantum = 0;
         }
     }
     
-    cout << "Quantum: " << quantum << " Input: " << input_file << "\n";
+    ifstream infile(input_file);
+    if (!infile) {
+        cerr << "Error: Cannot open input file\n";
+        return 1;
+    }
+
+    Process* head = nullptr;
+    Process* tail = nullptr;
+    int process_count = 0;
+    int id_counter = 1;
+    string line;
+
+    while (getline(infile, line)) {
+        if (line.empty()) continue;
+        
+        int colon1 = line.find(':');
+        int colon2 = line.find(':', colon1 + 1);
+        
+        if (colon1 != -1 && colon2 != -1) {
+            Process* p = new Process;
+            p->id = id_counter++;
+            p->burst = stoi(line.substr(0, colon1));
+            p->arrival = stoi(line.substr(colon1 + 1, colon2 - colon1 - 1));
+            p->priority = stoi(line.substr(colon2 + 1));
+            p->rem = p->burst;
+            p->wt = 0;
+            p->next = nullptr;
+            
+            if (!head) {
+                head = p;
+                tail = p;
+            } else {
+                tail->next = p;
+                tail = p;
+            }
+            process_count++;
+        }
+    }
+    infile.close();
+    cout << "Successfully parsed " << process_count << " processes. "<<quantum<<")\n";
 return 0;
 }
